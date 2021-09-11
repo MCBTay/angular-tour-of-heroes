cbafb83f3b0ede3e160de6233bc7ce441c385c8e
Bryan Taylor <mcbtay@gmail.com>
2021-09-10 23:40:11 -0400

    updating both getHero and getHeroes to go through http client and have some basic error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,13 +21,18 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.log(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+          tap(_ => this.log(`fetched hero id=${id}`)),
+          catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
   }
 
   private log(message: string) {
