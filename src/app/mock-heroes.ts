029ed70380559094b0814b5e53ba3e7071f9b1f9
Bryan Taylor <mcbtay@gmail.com>
2021-09-19 21:58:35 -0400

    updating mock hero data

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 11, name: 'Dr Nice', power: 'Really Smart', alterEgo: 'Mr. Nice Guy' },
+  { id: 12, name: 'Narco', power: 'Super Flexible', alterEgo: undefined},
+  { id: 13, name: 'Bombasto', power: 'Super Hot', alterEgo: undefined },
+  { id: 14, name: 'Celeritas', power: 'Weather Changer', alterEgo: 'Mr. Steal Yo Girl' },
+  { id: 15, name: 'Magneta', power: 'Super Hot', alterEgo: undefined },
+  { id: 16, name: 'RubberMan', power: 'Super Flexible', alterEgo: undefined },
+  { id: 17, name: 'Dynama', power: 'Really Smart', alterEgo: undefined },
+  { id: 18, name: 'Dr IQ', power: 'Really Smart', alterEgo: undefined },
+  { id: 19, name: 'Magma', power: 'Super Hot', alterEgo: 'Lava' },
+  { id: 20, name: 'Tornado', power: 'Weather Changer', alterEgo: 'Whirlwind' },
 ];
