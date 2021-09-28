094b2f22b5d096b1b5fd67457cc89eedd8c1d96d
Bryan Taylor <mcbtay@gmail.com>
2021-09-28 14:47:33 -0400

    updating to use a material design style navigation bar

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -3,12 +3,14 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule {}
