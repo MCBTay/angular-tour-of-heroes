abb2482808a2f9c9c4e5443be1e76f7fbd6a120b
Bryan Taylor <mcbtay@gmail.com>
2021-09-28 15:17:19 -0400

    updating dashboard for material design

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
-import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
